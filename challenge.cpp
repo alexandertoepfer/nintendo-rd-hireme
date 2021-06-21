dfe3d5d53725444c8072b1726d25d0a1063d1807
Alexander Töpfer <74869642+alexandertoepfer@users.noreply.github.com>
2021-06-21 11:10:34 +0200

    Update challenge.cpp

--- challenge.cpp
+++ challenge.cpp
@@ -73,7 +73,7 @@ void forward(u8 input[32], u8 output[32], u8 confusion[512], u32 diffusion[32])
 
 int main()
 {
-    u8 target[16] = "Hire me!!!!!!!!\0", output[32];
+    u8 target[16] = "Hire me!!!!!!!!", output[32];
     forward(input, output, confusion, diffusion);
     std::cout << output << std::endl;
     return memcmp(output, target, 16); // => contact jobs(at)nerd.nintendo.com
